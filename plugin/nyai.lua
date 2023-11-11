f0ae51e2d0f2524d32026d9909de2bb831f72f9f
anekos <anekos@snca.net>
2023-11-12 00:11:17 +0900

    Press `.` to run

--- plugin/nyai.lua
+++ plugin/nyai.lua
@@ -1,13 +1,13 @@
 local api = require('nyai.api')
 local buffer = require('nyai.buffer')
 
-vim.api.nvim_create_user_command('Nyai', function()
+local run = function()
   local current_buffer = vim.api.nvim_get_current_buf()
   local current_win = vim.api.nvim_get_current_win()
   local parameters = buffer.get_parameters()
 
   local on_resp = function(body)
-    local lines = { '', '<assistant>' }
+    local lines = { '<assistant>' }
 
     local last_line = vim.fn.getline('$')
     if last_line == '<WAITING>' then
@@ -29,11 +29,37 @@ vim.api.nvim_create_user_command('Nyai', function()
     vim.api.nvim_win_set_cursor(current_win, { num_lines, 0 })
   end
 
-  vim.api.nvim_buf_set_lines(current_buffer, -1, -1, true, { '<WAITING>' })
+  vim.api.nvim_buf_set_lines(current_buffer, -1, -1, true, { '', '<WAITING>' })
 
   api.chat_completions(parameters, function(body)
     vim.schedule(function()
       on_resp(vim.fn.json_decode(body))
     end)
   end)
+end
+
+local feedkeys = function(keys)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), 'n', false)
+end
+
+local group = vim.api.nvim_create_augroup('Nyai', { clear = true })
+
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  group = group,
+  pattern = 'nyai',
+  callback = function()
+    vim.keymap.set('i', '<CR>', function()
+      if vim.fn.getline('.') == '.' then
+        vim.schedule(function()
+          run()
+        end)
+      else
+        feedkeys('<CR>')
+      end
+    end, { expr = true, buffer = true })
+  end,
+})
+
+vim.api.nvim_create_user_command('Nyai', function()
+  run()
 end, {})
