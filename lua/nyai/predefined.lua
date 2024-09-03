00f07bc7343d7cde20d514b7328530d44ddb7edc
anekos <anekos@snca.net>
2024-09-03 17:19:21 +0900

    Implement `nyai.provider`

--- lua/nyai/predefined.lua
+++ lua/nyai/predefined.lua
@@ -1,87 +1,20 @@
+local provider = require('nyai.provider')
+
 local M = {}
 
--- https://platform.openai.com/docs/models
-M.OPENAI_ENDPOINT = 'https://api.openai.com/v1/chat/completions'
-
--- https://docs.perplexity.ai/reference/post_chat_completions
--- https://docs.perplexity.ai/guides/model-cards
-M.PERPLEXITY_ENDPOINT = 'https://api.perplexity.ai/chat/completions'
-
-M.GITHUB_COPILOT_ENDPOINT = 'https://api.githubcopilot.com/chat/completions'
-
 M.models = {
-  {
-    name = 'OpenAI - GPT 4o',
-    id = 'gpt-4o',
-    api_endpoint = M.OPENAI_ENDPOINT,
-    api_key = vim.env.OPENAI_API_KEY,
-  },
-  {
-    name = 'OpenAI - GPT 4o mini',
-    id = 'gpt-4o-mini',
-    api_endpoint = M.OPENAI_ENDPOINT,
-    api_key = vim.env.OPENAI_API_KEY,
-  },
-  {
-    name = 'OpenAI - GPT 4 Turbo',
-    id = 'gpt-4-turbo',
-    api_endpoint = M.OPENAI_ENDPOINT,
-    api_key = vim.env.OPENAI_API_KEY,
-  },
-  {
-    name = 'OpenAI - GPT 3.5 Turbo',
-    id = 'gpt-3.5-turbo',
-    api_endpoint = M.OPENAI_ENDPOINT,
-    api_key = vim.env.OPENAI_API_KEY,
-  },
-  {
-    name = 'Perplexity - Sonar Small Online 8B',
-    id = 'llama-3.1-sonar-small-128k-online',
-    api_endpoint = M.PERPLEXITY_ENDPOINT,
-    api_key = vim.env.PERPLEXITY_API_KEY,
-  },
-  {
-    name = 'Perplexity - Sonar Large Online 70B',
-    id = 'llama-3.1-sonar-small-128k-online',
-    api_endpoint = M.PERPLEXITY_ENDPOINT,
-    api_key = vim.env.PERPLEXITY_API_KEY,
-  },
-  {
-    name = 'Perplexity - Sonar Huge Online 405B',
-    id = 'llama-3.1-sonar-huge-128k-online',
-    api_endpoint = M.PERPLEXITY_ENDPOINT,
-    api_key = vim.env.PERPLEXITY_API_KEY,
-  },
-  {
-    name = 'Perplexity - Chat Small 8B',
-    id = 'llama-3.1-sonar-small-128k-chat',
-    api_endpoint = M.PERPLEXITY_ENDPOINT,
-    api_key = vim.env.PERPLEXITY_API_KEY,
-  },
-  {
-    name = 'Perplexity - Chat Large 70B',
-    id = 'llama-3.1-sonar-large-128k-chat',
-    api_endpoint = M.PERPLEXITY_ENDPOINT,
-    api_key = vim.env.PERPLEXITY_API_KEY,
-  },
-  {
-    name = 'Perplexity - Instruct Small 8B',
-    id = 'llama-3.1-8b-instruct',
-    api_endpoint = M.PERPLEXITY_ENDPOINT,
-    api_key = vim.env.PERPLEXITY_API_KEY,
-  },
-  {
-    name = 'Perplexity - Instruct Large 70B',
-    id = 'llama-3.1-70b-instruct',
-    api_endpoint = M.PERPLEXITY_ENDPOINT,
-    api_key = vim.env.PERPLEXITY_API_KEY,
-  },
-  {
-    name = 'Github - Copilot',
-    id = nil,
-    api_endpoint = M.GITHUB_COPILOT_ENDPOINT,
-    api_key = vim.env.GITHUB_TOKEN,
-  },
+  provider.openai('OpenAI - GPT 4o', 'gpt-4o'),
+  provider.openai('OpenAI - GPT 4o mini', 'gpt-4o-mini'),
+  provider.openai('OpenAI - GPT 4 Turbo', 'gpt-4-turbo'),
+  provider.openai('OpenAI - GPT 3.5 Turbo', 'gpt-3.5-turbo'),
+  provider.perplexity('Perplexity - Sonar Small Online 8B', 'llama-3.1-sonar-small-128k-online'),
+  provider.perplexity('Perplexity - Sonar Large Online 70B', 'llama-3.1-sonar-small-128k-online'),
+  provider.perplexity('Perplexity - Sonar Huge Online 405B', 'llama-3.1-sonar-huge-128k-online'),
+  provider.perplexity('Perplexity - Chat Small 8B', 'llama-3.1-sonar-small-128k-chat'),
+  provider.perplexity('Perplexity - Chat Large 70B', 'llama-3.1-sonar-large-128k-chat'),
+  provider.perplexity('Perplexity - Instruct Small 8B', 'llama-3.1-8b-instruct'),
+  provider.perplexity('Perplexity - Instruct Large 70B', 'llama-3.1-70b-instruct'),
+  provider.openai('Github - Copilot', nil),
 }
 
 return M
---
+++ lua/nyai/provider.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+-- https://platform.openai.com/docs/models
+M.OPENAI_ENDPOINT = 'https://api.openai.com/v1/chat/completions'
+
+-- https://docs.perplexity.ai/reference/post_chat_completions
+-- https://docs.perplexity.ai/guides/model-cards
+M.PERPLEXITY_ENDPOINT = 'https://api.perplexity.ai/chat/completions'
+
+M.GITHUB_COPILOT_ENDPOINT = 'https://api.githubcopilot.com/chat/completions'
+
+function M.openai(name, id)
+  return {
+    name = name,
+    id = id,
+    api_endpoint = M.OPENAI_ENDPOINT,
+    api_key = vim.env.OPENAI_API_KEY,
+  }
+end
+
+function M.perplexity(name, id)
+  return {
+    name = name,
+    id = id,
+    api_endpoint = M.PERPLEXITY_ENDPOINT,
+    api_key = vim.env.PERPLEXITY_API_KEY,
+  }
+end
+
+function M.copilot(name)
+  return {
+    name = name,
+    id = nil,
+    api_endpoint = M.COPILOT_ENDPOINT,
+    api_key = vim.env.GITHUB_TOKEN,
+  }
+end
+
+return M
