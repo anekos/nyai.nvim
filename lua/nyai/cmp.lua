6ecb6e2f907128181a6b9d1cc7f3a5127e8334ad
anekos <anekos@snca.net>
2024-09-08 19:50:03 +0900

    Refine

--- lua/nyai/cmp.lua
+++ lua/nyai/cmp.lua
@@ -35,6 +35,47 @@ local function get_current_model()
   return state.default_model()
 end
 
+local function complte_model_names(callback)
+  callback {
+    items = vim.tbl_map(function(name)
+      return {
+        label = name,
+        kind = kind.Value,
+      }
+    end, model_names),
+  }
+end
+
+local function complete_list(callback, values)
+  callback(vim.tbl_map(function(name)
+    return {
+      label = name,
+      kind = kind.Value,
+    }
+  end, values))
+end
+
+local function complete_boolean(callback)
+  callback {
+    { label = 'true', kind = kind.Value },
+    { label = 'false', kind = kind.Value },
+  }
+end
+
+local function complete_model_parameters(callback, model_parameters)
+  local items = { { label = '@model', kind = kind.Property } }
+
+  for key, value in pairs(model_parameters) do
+    table.insert(items, {
+      label = vim.fn.printf('@%s (%s)', key, value),
+      kind = kind.Property,
+      insertText = '@' .. key,
+    })
+  end
+
+  callback(items)
+end
+
 function M:complete(params, callback)
   local ctx = params.context
 
@@ -46,37 +87,30 @@ function M:complete(params, callback)
   local model = get_current_model()
 
   if ctx.cursor_before_line:match('^@model%s*=%s*') then
-    callback {
-      items = vim.tbl_map(function(name)
-        return {
-          label = name,
-          kind = kind.Value,
-        }
-      end, model_names),
-    }
-    return
+    return complte_model_names(callback)
   end
 
-  if ctx.cursor_before_line:match('^@[%s%w_]+=') then
+  local parameter_name = ctx.cursor_before_line:match('^@([%w_]+)%s*=')
+
+  if parameter_name then
+    if model then
+      local value = model.parameters[parameter_name]
+      if type(value) == 'table' then
+        return complete_list(callback, value)
+      elseif type(value) == 'boolean' then
+        return complete_boolean(callback)
+      end
+      return
+    end
+
     callback {}
     return
   end
 
   if ctx.cursor_before_line:match('^@') then
-    local items = { { label = '@model', kind = kind.Property } }
-
     if model and model.parameters then
-      for key, value in pairs(model.parameters) do
-        table.insert(items, {
-          label = vim.fn.printf('@%s (%s)', key, value),
-          kind = kind.Property,
-          insertText = '@' .. key,
-        })
-      end
+      return complete_model_parameters(callback, model.parameters)
     end
-
-    callback { items = items }
-    return
   end
 
   callback {}
