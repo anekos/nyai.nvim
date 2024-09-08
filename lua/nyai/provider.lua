61faf0fc66573c9011c8cd7cd1142cc78f3528c7
anekos <anekos@snca.net>
2024-09-08 19:05:51 +0900

    Add parameter definitions

--- lua/nyai/provider.lua
+++ lua/nyai/provider.lua
@@ -15,6 +15,26 @@ function M.openai(name, id)
     id = id,
     api_endpoint = M.OPENAI_ENDPOINT,
     api_key = vim.env.OPENAI_API_KEY,
+    -- https://platform.openai.com/docs/api-reference/chat
+    parameters = {
+      frequency_penalty = 'float',
+      logit_bias = 'table',
+      logprobs = 'boolean',
+      top_logprobs = 'integer',
+      max_tokens = 'integer',
+      -- n = 'integer',
+      presence_penalty = 'float',
+      -- response_format
+      seed = 'integer',
+      -- service_tier = 'string,
+      stop = 'string | string[]',
+      stream = 'boolean',
+      temperature = 'float',
+      top_p = 'float',
+      -- tools = 'array',
+      -- tool_choice = 'string',
+      -- parallel_tool_calls = 'boolean',
+    },
   }
 end
 
@@ -24,6 +44,20 @@ function M.perplexity(name, id)
     id = id,
     api_endpoint = M.PERPLEXITY_ENDPOINT,
     api_key = vim.env.PERPLEXITY_API_KEY,
+    -- https://docs.perplexity.ai/api-reference/chat-completions
+    parameters = {
+      max_tokens = 'integer',
+      temperature = 'float',
+      top_p = 'float',
+      return_citations = 'boolean',
+      search_domain_filter = 'string[]',
+      -- return_images = 'boolean',
+      -- return_related_questions = 'boolean',
+      search_recency_filter = { 'month', 'week', 'day', 'hour' },
+      top_k = 'integer', -- 0 to 2048 inclusive
+      presence_penalty = 'float', -- -2.0 to 2.0
+      frequency_penalty = 'float', -- 0 to
+    },
   }
 end
 
