103d8d8ba6b35b86145a0f44935722b389642462
anekos <anekos@snca.net>
2024-09-12 15:57:03 +0900

    Remove old implementation

--- lua/nyai/api.lua
+++ lua/nyai/api.lua
@@ -38,25 +38,6 @@ function M.chat_completions(request, callbacks)
     body = vim.fn.json_encode(params),
     timeout = 60 * 1000,
     stream = request.model.stream(callbacks),
-    -- callback = function(resp)
-    --   -- if resp.status ~= 200 then
-    --   --   error('Failed to request chat completions: ' .. resp.body)
-    --   -- end
-    --   --
-    --   -- vim.schedule(function()
-    --   --   local body = vim.fn.json_decode(resp.body)
-    --   --
-    --   --   -- Ollama
-    --   --   -- https://github.com/ollama/ollama/blob/main/docs/api.md
-    --   --   if body.message and body.message.content then
-    --   --     callback(body.message.content)
-    --   --     return
-    --   --   end
-    --   --
-    --   --   -- Open AI
-    --   --   callback(body.choices[1].message.content)
-    --   -- end)
-    -- end,
   }
 end
 
