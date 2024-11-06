b21b38fc79da725930cdd3b2a08273c2856176e9
anekos <anekos@snca.net>
2024-11-06 22:14:40 +0900

    Implement cmp completion

---
+++ lua/nyai/cmp/completion.lua
@@ -0,0 +1,59 @@
+local config = require('nyai.config')
+local api = require('nyai.api')
+
+local M = {}
+
+M.candidates = {}
+
+function M.new()
+  return setmetatable({}, { __index = M })
+end
+
+function M.get_keyword_pattern()
+  return [[\k\+]]
+end
+
+function M.is_available()
+  return config.cmp_model ~= nil
+end
+
+function M:complete(params, callback)
+  local ctx = params.context
+
+  local prev = ctx.prev_context
+  local ln = prev.cursor.line
+
+  local prefix = vim.fn.join(vim.api.nvim_buf_get_lines(ctx.bufnr, 1, ln, true), '\n')
+    .. '\n'
+    .. prev.cursor_before_line
+  local suffix = '\n' .. vim.fn.join(vim.api.nvim_buf_get_lines(ctx.bufnr, ln + 1, -1, true), '\n')
+
+  if config.cmp_model == nil then
+    error('WTF')
+  end
+
+  local p = config.cmp_model().request(prefix, suffix)
+
+  local request = {
+    url = p.url,
+    headers = p.headers,
+    body = p.body,
+    response = function()
+      return {
+        on_response = function(data)
+          callback {
+            {
+              label = 'nyai',
+              insertText = data.response,
+            },
+          }
+        end,
+        on_complete = function() end,
+      }
+    end,
+  }
+
+  api.call(request, nil)
+end
+
+return M
